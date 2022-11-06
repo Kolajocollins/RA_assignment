4df5e2bfe27d1ec386ef14c79fc36ae8e03db74d
Kolajocollins <oluwakolajocollins@gmail.com>
2022-11-06 22:37:15 +0100

    Implemented the euclidean algorithm using recursion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+//the code below finds the modular multiplicative inverse x of of an  
+
+
+
+fn egcd(a:i64,b:i64) ->(i64,i64,i64)
+{
+    if a==0
+    {
+        (b,0,1)
+    }
+    else
+    {
+        let(g,x,y)= egcd(b%a,a);
+        let d = y - (b / a) * x;
+        (g,d,x)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let (a,b,c)= egcd(42823,6409);
+    println!("Hello, world!={}",a);
 }
